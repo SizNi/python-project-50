5925690fee04a74e8d5443ed06f1e22dd0f80a7c
Nik Sizov <wwwwwwwww@list.ru>
2022-10-28 04:15:59 +0300

    add -f (format) option

--- hexlet-code/__init__.py
+++ hexlet-code/__init__.py
@@ -6,6 +6,7 @@ def main():
     parser = argparse.ArgumentParser(prog = 'gendiff', description = description)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
 
 if __name__ == '__main__':
