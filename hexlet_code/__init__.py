5bc50a2931cc50a3f9eb2b1467ebbd8d0a18bed9
Nik Sizov <wwwwwwwww@list.ru>
2022-11-05 03:27:25 +0300

    add version output

--- hexlet_code/__init__.py
+++ hexlet_code/__init__.py
@@ -7,9 +7,12 @@ def main():
     parser = argparse.ArgumentParser(prog='gendiff', description=description)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-v', '--version', action='version',
+                        version='Current version is 1.0')
     parser.add_argument(
-        '-f', '--format', help='set format of output', default='stylish'
-        )
+        '-f', '--format', choices=['stylish', 'plain', 'json'],
+        help='output format(default: stylish)', default='stylish'
+    )
     args = parser.parse_args()
     result = generate_diff(args.first_file, args.second_file, args.format)
     print(result)
